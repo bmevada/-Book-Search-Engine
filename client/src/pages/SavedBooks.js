ad7154000c58c5c05e17206d723ae5274fc6ed94
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-12-29 19:35:39 +1100

    amendments to save and search books

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -4,9 +4,16 @@ import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import {useMutation, useQuery} from '@apollo/react-hooks';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  // const [userData, setUserData] = useState({});
+  const { loading, data} = useQuery(GET_ME);
+  const [removeBook, {error}] = useMutation(REMOVE_BOOK);
+
+  const userData = data?.me || [];
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
